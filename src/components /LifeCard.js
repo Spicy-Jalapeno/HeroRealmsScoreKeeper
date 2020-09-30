e23bd2932a65e48781e0a39e26f08e288c253601
Alexis Hernandez <jalapeno9597@gmail.com>
2020-09-29 21:53:52 -0500

    Added the icon to the button and tested for Iphone comp.

--- src/components /LifeCard.js
+++ src/components /LifeCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Tile, Button} from 'react-native-elements';
 import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const styles = StyleSheet.create({
   // tile: {
@@ -20,10 +21,14 @@ const styles = StyleSheet.create({
   upBut: {
     height: 100,
     width: 100,
+    alignItems: 'center',
+    backgroundColor: 'transparent',
   },
   downBut: {
     height: 100,
     width: 100,
+    alignItems: 'center',
+    backgroundColor: 'transparent',
   },
   score: {
     fontSize: 100,
@@ -46,20 +51,28 @@ const LifeCard = (props) => {
   };
   return (
     <Tile
-      contentContainerStyle={styles.tileContent ,{backgroundColor: props.color}}
+      contentContainerStyle={
+        (styles.tileContent, {backgroundColor: props.color})
+      }
       containerStyle={props.position}
       // imageContainerStyle={styles.tile}
       // imageSrc={props.imgsrc}
       // featured
     >
       <View style={styles.tileContent}>
-        <Button buttonStyle={styles.upBut} onPress={handleInc}>
-          Up
-        </Button>
+        <Button
+          buttonStyle={styles.upBut}
+          onPress={handleInc}
+          icon={<Icon name="arrow-up" size={90} />}
+        />
+
         <Text style={styles.score}>{num}</Text>
-        <Button buttonStyle={styles.downBut} onPress={handleDec}>
-          Down
-        </Button>
+        <Button
+          buttonStyle={styles.downBut}
+          onPress={handleDec}
+          icon={<Icon name="arrow-down" size={90} />}
+        />
+
       </View>
     </Tile>
   );
